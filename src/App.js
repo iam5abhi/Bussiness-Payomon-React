a8a52cda9936abec750bad6401b3fb3fca6c1db7
Abhinandan Jhamb <77092975+iam5abhi@users.noreply.github.com>
2022-09-28 12:52:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Account from "./Components/Account";
+// import AddAccount from "./Components/AddAccount";
+// import TransferBank from "./Components/TransferBank";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import PrivateRoute from './routes/PrivateRoutes'
+import ForgetPassword from "./Components/ForgetPassword";
+// import PaymentProcess from "./Components/PaymentProcess";
+import PasswordReset from "./Components/PasswordReset";
+import { Routes,Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    
+    <>
+      <Routes>
+        <Route path='/' element={<PrivateRoute><Account/></PrivateRoute> }/>
+        <Route path='/forgetpassword' element={ < ForgetPassword/> } />
+        <Route path='/passwordreset' element={ < PasswordReset/> } />
+        <Route path='/signup' element={ < Signup />} />
+        <Route path='/login' element={ < Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
